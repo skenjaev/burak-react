31c5ba3b80e7c4fa0b1bb802ff0e32c00374efd6
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-08-21 20:38:04 +0900

    feat: middleware integration redux logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action, getDefaultMiddleware } from "@reduxjs/toolkit";
 import homePageReducer from "./screens/homePage/slice";
 import HomePageReducer from "./screens/homePage/slice";
-
+import reduxLogger from "redux-logger";
 export const store = configureStore({
+  // @ts-ignore
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxLogger),
   reducer: {
   homePage: homePageReducer,
   },
